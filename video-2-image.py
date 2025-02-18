4bf8f81b69fc85a4a294ae699facc5423e3c3d75
haotsai101 <tsaizhihao@gmail.com>
2025-02-18 15:25:37 -0700

    Ty/Update video-2-image script to resize the image to facelib compatible size

--- video-2-image.py
+++ video-2-image.py
@@ -3,36 +3,45 @@ import numpy as np
 import argparse
 import cv2
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process video to images.')
+    parser.add_argument('--filename', type=str, required=True, help='MP4 file in /data folder')
+    parser.add_argument('--resolution', type=int, default=0, help='Target height to resize; 0 means no resize')
+    return parser.parse_args()
+
 def main():
-  source = 'data/input.mp4'
-  dest_folder = 'data/frames'
+    args = parse_args()
+    source = os.path.join('data', args.filename)
+    dest_folder = 'data/frames'
 
-  if not os.path.exists(dest_folder):
-      os.makedirs(dest_folder)
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
 
-  cap = cv2.VideoCapture(source)
-  path_to_save = os.path.abspath(dest_folder)
-  
-  current_frame = 1
+    cap = cv2.VideoCapture(source)
+    path_to_save = os.path.abspath(dest_folder)
+    
+    current_frame = 1
 
-  if (cap.isOpened() == False):
-      print('Cap is not open')
+    if not cap.isOpened():
+        print('Cap is not open')
 
-  while(cap.isOpened()):
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            if args.resolution > 0:
+                h, w = frame.shape[:2]
+                aspect_ratio = w / h
+                target_w = int(aspect_ratio * args.resolution)
+                frame = cv2.resize(frame, (target_w, args.resolution), interpolation=cv2.INTER_AREA)
 
-      ret, frame = cap.read()
-      if(ret == True):
+            name = f'frame{current_frame}.jpg'
+            cv2.imwrite(os.path.join(path_to_save, name), frame)
+            current_frame += 1
+        else:
+            break
 
-          name = 'frame' + str(current_frame) + '.jpg'
-          cv2.imwrite(os.path.join(path_to_save, name), frame)
-
-          current_frame += 1
-      
-      else:
-          break
-
-  cap.release()
-  print('done')
+    cap.release()
+    print('done')
 
 if __name__ == '__main__':
-  main()
+    main()
